68ce28f14d20303f562823c5e0d05207264a6507
Luiz Orlandi <orandiluiz@gmail.com>
2024-08-01 21:05:41 -0300

    Exercicio 1 (E1) pronto

---
+++ Exercicio OOP (E1)/E1/Aluno.cs
@@ -0,0 +1,94 @@
+﻿using System;
+namespace E1
+{
+    public class Universidade
+    {
+        // Método normal
+        public void Ensino()
+        {
+            Console.WriteLine("Método de ensino");
+        }
+
+        // Método sobreescrita
+        public class Guardar
+        {
+            public virtual void Finalidade()
+            {
+                Console.WriteLine("Mochila (Armazenar)");
+            }
+        }
+
+        // Para executar a sobrescrita do método Finalidade que está no objeto pai, utilizamos o override
+        public class GuardarMateriais : Guardar
+        {
+            public override void Finalidade()
+            {
+                base.Finalidade();
+                Console.WriteLine("Manter os materiais organizados");
+            }
+        }
+
+        // Método sobrecarga
+        public virtual void SomIntervalo()
+        {
+            Console.WriteLine("Todos para o intervalo");
+        }
+
+
+        // Assinaturas
+        public void AbrirMochila()
+        {
+            Console.WriteLine("Mochila está sendo aberta...");
+        }
+
+        public bool AbrirMochila(string mochila)
+        {
+            Console.WriteLine($"Abrindo a mochila {mochila}");
+            return true;
+        }
+
+        // Construtor padrão
+        public class Universitario
+        {
+            // Atributos
+            private string Sobrenome { get; set; }
+
+            // Propriedades
+            public string Nome { get; set; }
+
+            public Universitario(string nome)
+            {
+                Nome = nome;
+            }
+
+            public void Estudar()
+            {
+                Console.WriteLine($"{Nome} está estudando.");
+            }
+        }
+
+        // Classe filha (sub classe)
+        public class Patrimonio : Universidade
+        {
+            private string _numeracao;
+            private string _nome;
+
+            public string Nome
+            {
+                get { return _nome; }
+                set { _nome = value; }
+            }
+
+            public string Numeracao
+            {
+                get { return _numeracao; }
+                set { _numeracao = value; }
+            }
+
+            public void ExibirPatrimonio()
+            {
+                Console.WriteLine($"Patrimônio: {Nome}, Cor: {Numeracao}");
+            }
+        }
+    }
+}
---
+++ Exercicio OOP (E1)/E1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using E1;
+
+var universitario = new Universidade.Universitario("Luiz");
+universitario.Estudar();
+universitario.Nome = "Carlos";
+universitario.Estudar();
+Console.WriteLine($"Nome do universitário: {universitario.Nome}");
+
+var mochila = new Universidade.GuardarMateriais();
+mochila.Finalidade();
+
+var universidade = new E1.Universidade();
+universidade.AbrirMochila();
+universidade.AbrirMochila("Mochila Verde");
+
+var patrimonio = new Universidade.Patrimonio
+{
+    Nome = "Biblioteca",
+    Numeracao = "56818BILB1659"
+};
+patrimonio.ExibirPatrimonio();
